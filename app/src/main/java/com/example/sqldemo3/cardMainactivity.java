93f9f51647635f0e78bf7624a378dcc7c8ac4549
Sascha <42777981+SaschaCoffee@users.noreply.github.com>
2022-10-25 22:39:18 +0200

    card firebase geht

--- app/src/main/java/com/example/sqldemo3/cardMainactivity.java
+++ app/src/main/java/com/example/sqldemo3/cardMainactivity.java
@@ -7,24 +7,36 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class cardMainactivity extends AppCompatActivity {
 
+    private FirebaseUser user;
+    private DatabaseReference reference;
     ArrayList<greenCardModel> lstBook = new ArrayList<>();
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private ProgressBar pgBar;
     private TextView tv;
-    int progr = 0;
+    int progr;
     private Button btn_add_data, btn_upload_data;
     private AlertDialog dialog;
+    private String userid;
 
 
     @Override
@@ -34,6 +46,34 @@ public class cardMainactivity extends AppCompatActivity {
         pgBar = findViewById(R.id.progress_bar);
         tv = findViewById(R.id.text_view_progress);
         btn_add_data = findViewById(R.id.btn_add_data);
+        String childcard = "anzahl";
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("cardprogress");
+        userid = user.getUid();
+
+
+
+        reference.child(childcard).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+            try {
+                long model = snapshot.getValue(long.class);
+                progr = Integer.parseInt(String.valueOf(model));
+
+                updateCard(progr);
+                buildRecyclerView();
+            }catch (Exception e){
+                Toast.makeText(cardMainactivity.this, "dd", Toast.LENGTH_SHORT).show();
+            }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -42,7 +82,6 @@ public class cardMainactivity extends AppCompatActivity {
         View view = getLayoutInflater().inflate(R.layout.custom_dialog, null);
 
         btn_upload_data = view.findViewById(R.id.btn_upload_data);
-
 
         createCard();
         buildRecyclerView();
@@ -59,11 +98,11 @@ public class cardMainactivity extends AppCompatActivity {
         btn_upload_data.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progr++;
+                reference.child("anzahl").setValue(progr);
                 updateProgressBar(progr);
                 updateCard(progr);
                 buildRecyclerView();
-                progr++;
-
                 dialog.dismiss();
 
             }
@@ -81,8 +120,10 @@ public class cardMainactivity extends AppCompatActivity {
 
     private void updateCard(int z) {
         int countstart = z;
-        lstBook.set(countstart, new greenCardModel(R.drawable.quadratgruen));
-        Toast.makeText(this, "" + countstart, Toast.LENGTH_SHORT).show();
+        for (int x = 0; x < z; x++) {
+            lstBook.set(x, new greenCardModel(R.drawable.quadratgruen));
+            Toast.makeText(this, "" + countstart, Toast.LENGTH_SHORT).show();
+        }
 
     }
 
---
+++ app/src/main/java/com/example/sqldemo3/cardModel.java
@@ -0,0 +1,17 @@
+package com.example.sqldemo3;
+
+public class cardModel {
+   String anzahl;
+
+    public cardModel(String anzahl) {
+        this.anzahl = anzahl;
+    }
+
+    public String getAnzahl() {
+        return anzahl;
+    }
+
+    public void setAnzahl(String anzahl) {
+        this.anzahl = anzahl;
+    }
+}
